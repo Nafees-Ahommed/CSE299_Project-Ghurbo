f4696a6ab63de714ded97019f4227ee656d5ed9a
MdRakibul726 <149356728+MdRakibul726@users.noreply.github.com>
2025-06-30 10:10:56 +0600

    Add files via upload
    PakageDetails.jsx

---
+++ Ghurbo/client/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import TourPackages from './pages/TourPackages';
+import PackageDetails from './pages/PackageDetails';
+
+
+const App = () => {
+
+ 
+  const isOwnerPath= useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+
+      {!isOwnerPath && <Navbar />}
+
+      <div className='min-h-[70vh]'>
+
+      <Routes>
+
+        <Route path= '/' element ={<Home/>} />
+        <Route path= '/packages' element ={<TourPackages/>} />
+        <Route path= '/packages/:id' element ={<PackageDetails/>} />
+
+      </Routes>
+
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
